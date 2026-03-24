c251bdfaac288e5106439425aa2d2081c4141120
Raj Zite <rajzite08@gmail.com>
2026-03-24 20:41:12 +0530

    Initial commit

---
+++ backend/jobtracker/src/test/java/com/smart/jobtracker/JobtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.jobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
